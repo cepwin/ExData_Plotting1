4665be030c3b0b4c8cb03933be965ec7204997ff
cepwin <sarrett@verizon.net>
2016-08-31 20:52:06 -0400

    initial work

---
+++ Plot1.R
@@ -0,0 +1,9 @@
+Plot1<-function() {
+tab1<-read.table(file="household_power_consumption.txt",header = TRUE,na.strings="?",sep=";")
+tab1$DateTime<-with(tab1,paste0(Date," ",Time))
+tab1$DateTime<-strptime(tab1$DateTime,format="%d/%m/%Y %H:%M:%S")
+tab2<-subset(tab1,DateTime>="2007-02-01" & DateTime<"2007-02-03")
+par(mfrow=c(1,1))
+hist(tab2$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+tab2
+}
